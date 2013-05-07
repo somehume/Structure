dcbdfd53c340b05ffc74d1603d1e47a119bb67ab
Donald Brown <donnie@Brown.local>
2013-05-07 14:34:32 -0400

    Initial Commit

---
+++ Structure/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Structure
+//
+//  Created by Donald Brown on 5/7/13.
+//  Copyright (c) 2013 Donald Brown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
